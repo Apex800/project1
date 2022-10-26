87f37ac5bc9d0fadf7226dd4faa17e3819058331
apex <khanammar800@gmail.com>
2022-10-26 07:48:17 +0530

    completed grid

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,54 @@
-function toggleimage(){
+function toggleimage() 
+{
     var a = document.getElementById("b1");
-    var b = document.getElementById("img1");
+    var c = document.getElementById("img2");
+    var d = document.getElementById("img3");
+    var e = document.getElementById("img4");
+    var f = document.getElementsByClassName("Card_text")
     
-    if(b.style.display === "none"){
-        b.style.display = "block";
-        a.innerHTML = "Hide";
+    
+        if(c.style.display === "none")
+    {
+        c.style.display = "block";
+        a.innerHTML = "Hide All";
     }
     else{
-        b.style.display = "none";
-        a.innerHTML = "Show";
+        c.style.display = "none";
+        a.innerHTML = "Show All";
+    }
+    
+    
+    
+        if(d.style.display === "none")
+    {
+        d.style.display = "block";
+        a.innerHTML = "Hide All";
+    }
+    else{
+        d.style.display = "none";
+        a.innerHTML = "Show All";
+    }
+    
+    
+    
+        if(e.style.display === "none")
+    {
+        e.style.display = "block";
+        a.innerHTML = "Hide All";
+    }
+    else{
+        e.style.display = "none";
+        a.innerHTML = "Show All";
+    }
+    
+    
+    if(f.style.display === "none")
+    {
+        f.style.display = "block";
+        a.innerHTML = "Hide All";
+    }
+    else{
+        f.style.display = "none";
+        a.innerHTML = "Show All";
     }
 }
